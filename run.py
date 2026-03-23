234bd8e4cc388fe5b0d29caab1875c2ab6475c2f
guille001-star <geopat001@gmail.com>
2026-03-23 17:51:08 -0300

    Sincronizar configuracion y agregar branding

--- run.py
+++ run.py
@@ -1,7 +1,4 @@
-﻿from app import create_app, db
+﻿from app import create_app
 app = create_app()
-with app.app_context():
-    try: db.create_all(); print(">>> DB OK")
-    except: pass
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
